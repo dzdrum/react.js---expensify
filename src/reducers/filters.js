a0d3029c49a7fd33bc9a1348d7e2ffda81c28d3b
dzdrum <dzdrum12@gmail.com>
2018-06-29 09:03:07 -0400

    finished wiring up adding and editing expense. Also added the ability to filter by dates on the dashboard

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,12 +1,14 @@
+import moment from 'moment';
+
 // Filters Reducer
 
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
   };
-  
+
   export default (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
       case 'SET_TEXT_FILTER':
